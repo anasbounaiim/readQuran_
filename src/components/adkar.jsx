de7395ca2cdf01f30a0fb26da8951f899d291f44
anas <anasbounaiim1@gmail.com>
2023-11-13 22:44:59 +0100

    fix bugs

--- src/components/adkar.jsx
+++ src/components/adkar.jsx
@@ -4,41 +4,39 @@ import axios from 'axios';
 const PrayerCarousel = () => {
   const [prayers, setPrayers] = useState([]);
 
+  const apiUrl = "https://api.jsonbin.io/v3/b/654ccdef12a5d37659970682"
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const fetchPrayers = async () => {
-      try {
-        const response = await axios.get('/data/prayers.json');
-        setPrayers(response.data);
-      } catch (error) {
-        console.error('Error fetching prayers:', error);
-      }
-    };
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setApiData(data.record);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching prayer timings data:", error);
+        setApiData([]);
+        setLoading(false);
+      });
+  }, [apiUrl]);
 
-    fetchPrayers();
-  }, []);
-
+console.log(apiData)
   return (
-    <div className="max-w-md mx-auto p-4">
-      <div id="prayerCarousel" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          {prayers.map((prayer, index) => (
-            <div key={prayer.id} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-              <h2 className="text-2xl font-bold mb-4">{prayer.name}</h2>
-              <p className="text-lg">{prayer.data[0].text}</p>
-              <p className="italic">{prayer.data[0].disc}</p>
-            </div>
-          ))}
-        </div>
-        {/* Add navigation controls */}
-        <a className="carousel-control-prev" href="#prayerCarousel" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#prayerCarousel" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+    <div className="max-w-md mx-auto p-4 text-black">
+     <h1 className=''>Adkar</h1>
+
+     <ul >
+          <li>dd</li>
+        
+
+        </ul>
     </div>
   );
 };
